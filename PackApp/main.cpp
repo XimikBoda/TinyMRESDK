4fb8847bca97ce317723e13f7a223c54efaf3fde
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2023-11-21 00:12:26 +0200

    Some fixes and added permission ids (all for beginning)

--- PackApp/main.cpp
+++ PackApp/main.cpp
@@ -4,15 +4,34 @@
 #include <elfio/elfio.hpp>
 #include <cmdparser.hpp>
 
+typedef unsigned char byte_t;
+
+byte_t all_ids[208] = {
+	0x88, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x89, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x8A, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x8B, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x8C, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x8D, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x8E, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x8F, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x90, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x91, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x92, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x93, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x94, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x95, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x96, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x97, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x98, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x99, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x9A, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x9B, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x9C, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x9D, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0x9E, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x9F, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+	0xA0, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0xA1, 0x13, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00
+};
+
 using namespace std;
 using namespace ELFIO;
 
-void fix_res_offsets(vector<byte>& buf, size_t res_offset, size_t res_size);
-void add_tag(vector<byte>& buf, long id, const vector<byte>& data);
-vector<byte> string_to_vector(string name);
-vector<byte> mstring_to_vector(string name);
-vector<byte> int_to_vector(unsigned long num);
-void add_end(vector<byte>& buf, long tags_pos);
+
+void fix_res_offsets(vector<byte_t>& buf, size_t res_offset, size_t res_size);
+void add_tag(vector<byte_t>& buf, long id, const vector<byte_t>& data);
+vector<byte_t> string_to_vector(string name);
+vector<byte_t> mstring_to_vector(string name);
+vector<byte_t> int_to_vector(unsigned long num);
+void add_end(vector<byte_t>& buf, long tags_pos);
 
 int main(int argc, char** argv)
 {
@@ -40,7 +59,7 @@ int main(int argc, char** argv)
 	}
 
 	//Open res file
-	vector<byte> res_buf;
+	vector<byte_t> res_buf;
 	{
 		std::ifstream res_in(res_path, ios::in | ios::binary | ios::ate);
 		if (!res_in.is_open()) {
@@ -62,7 +81,7 @@ int main(int argc, char** argv)
 	note_sec->append_data((char*)res_buf.data(), res_buf.size());
 
 	//Getting buffer
-	vector<byte> full_file_buf;
+	vector<byte_t> full_file_buf;
 	{
 		stringstream ss;
 		reader.save(ss);
@@ -86,16 +105,17 @@ int main(int argc, char** argv)
 	add_tag(full_file_buf, 0x10, int_to_vector(0));
 	add_tag(full_file_buf, 0x11, int_to_vector(0x9C400000));
 	add_tag(full_file_buf, 0x12, string_to_vector("1234567890"));
+	add_tag(full_file_buf, 0x13, vector<byte_t>(begin(all_ids), end(all_ids)));
 	add_tag(full_file_buf, 0x16, int_to_vector(1));
 	add_tag(full_file_buf, 0x18, int_to_vector(0));
-	//add_tag(full_file_buf, 0x19, mstring_to_vector("App name"));
+	add_tag(full_file_buf, 0x19, mstring_to_vector("App name"));
 	add_tag(full_file_buf, 0x1C, int_to_vector(0));
 	add_tag(full_file_buf, 0x21, int_to_vector(6));
 	add_tag(full_file_buf, 0x22, int_to_vector(0));
 	add_tag(full_file_buf, 0x23, int_to_vector(0));
 	add_tag(full_file_buf, 0x25, int_to_vector(0));
 	add_tag(full_file_buf, 0x29, int_to_vector(0));
-	add_tag(full_file_buf, 0x00, vector<byte>());
+	add_tag(full_file_buf, 0x00, vector<byte_t>());
 
 	//Add end
 	add_end(full_file_buf, tag_pos);
@@ -107,7 +127,7 @@ int main(int argc, char** argv)
 	out.close();
 }
 
-void fix_res_offsets(vector<byte>& buf, size_t res_offset, size_t res_size)
+void fix_res_offsets(vector<byte_t>& buf, size_t res_offset, size_t res_size)
 {
 	size_t pos = res_offset;
 
@@ -147,14 +167,14 @@ void fix_res_offsets(vector<byte>& buf, size_t res_offset, size_t res_size)
 	}
 }
 
-void add_vector(vector<byte>& a, const vector<byte>& b)
+void add_vector(vector<byte_t>& a, const vector<byte_t>& b)
 {
 	a.insert(a.end(), b.begin(), b.end());
 }
 
-void add_tag(vector<byte>& buf, long id, const vector<byte>& data)
+void add_tag(vector<byte_t>& buf, long id, const vector<byte_t>& data)
 {
-	vector<byte> tag(8);
+	vector<byte_t> tag(8);
 	tag.resize(8);
 	*(long*)(tag.data()) = id;
 	*(long*)(tag.data() + 4) = data.size();
@@ -162,24 +182,24 @@ void add_tag(vector<byte>& buf, long id, const vector<byte>& data)
 	add_vector(buf, data);
 }
 
-vector<byte> string_to_vector(string name)
+vector<byte_t> string_to_vector(string name)
 {
-	vector<byte> str(4 + name.size() + 1);
+	vector<byte_t> str(4 + name.size() + 1);
 	memcpy(str.data() + 4, name.c_str(), name.size() + 1);
 	*(long*)(str.data()) = name.size() + 1;
 	return str;
 }
 
-vector<byte> int_to_vector(unsigned long num)
+vector<byte_t> int_to_vector(unsigned long num)
 {
-	vector<byte> n(4);
+	vector<byte_t> n(4);
 	*(unsigned long*)(n.data()) = num;
 	return n;
 }
 
-vector<byte> mstring_to_vector(string name)
+vector<byte_t> mstring_to_vector(string name)
 {
-	vector<byte> str;
+	vector<byte_t> str;
 	for (int i = 0; i < 3; ++i) {
 		add_vector(str, int_to_vector(i+1));
 		add_vector(str, string_to_vector(name));
@@ -188,11 +208,10 @@ vector<byte> mstring_to_vector(string name)
 	return str;
 }
 
-void add_end(vector<byte>& buf, long tags_pos)
+void add_end(vector<byte_t>& buf, long tags_pos)
 {
-	vector<byte> end(0x56);
-	unsigned char st_bytes[7] = { 0xB4, 0x56, 0x44, 0x45, 0x31, 0x30, 0x1 };
-	memcpy(end.data(), st_bytes, 7);
+	vector<byte_t> end= { 0xB4, 0x56, 0x44, 0x45, 0x31, 0x30, 0x1 };
+	end.resize(0x56);
 	*(long*)(end.data() + 0x56 - 12) = tags_pos;
 	add_vector(buf, end);
 }
