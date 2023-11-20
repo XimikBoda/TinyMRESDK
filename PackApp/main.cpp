42b300f4dc13b3055c9a7dadf91a086825f0dd57
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2023-11-20 23:59:40 +0200

    Add tags and ending

--- PackApp/main.cpp
+++ PackApp/main.cpp
@@ -7,7 +7,12 @@
 using namespace std;
 using namespace ELFIO;
 
-void fix_res_offsets(vector<byte> &buf, size_t res_offset, size_t res_size);
+void fix_res_offsets(vector<byte>& buf, size_t res_offset, size_t res_size);
+void add_tag(vector<byte>& buf, long id, const vector<byte>& data);
+vector<byte> string_to_vector(string name);
+vector<byte> mstring_to_vector(string name);
+vector<byte> int_to_vector(unsigned long num);
+void add_end(vector<byte>& buf, long tags_pos);
 
 int main(int argc, char** argv)
 {
@@ -17,12 +22,14 @@ int main(int argc, char** argv)
 	{
 		parser.set_required<std::string>("a", "axf", "Elf file from the compiler");
 		parser.set_required<std::string>("r", "res", "Resource File");
+		parser.set_required<std::string>("o", "out", "Out file");
 	}
 
 	parser.run_and_exit_if_error();
 
 	string axf_path = parser.get<std::string>("a");
 	string res_path = parser.get<std::string>("r");
+	string out_path = parser.get<std::string>("o");
 
 	//Open axf file
 	elfio reader;
@@ -64,10 +71,43 @@ int main(int argc, char** argv)
 		memcpy(full_file_buf.data(), ss.str().c_str(), ss.str().size());
 	}
 
+	//Fix res offsets
 	fix_res_offsets(full_file_buf, note_sec->get_offset(), note_sec->get_size());
+
+	long tag_pos = full_file_buf.size();
+
+	//Add tags
+	add_tag(full_file_buf, 0x01, string_to_vector("Develop name"));
+	add_tag(full_file_buf, 0x02, int_to_vector(-1));
+	add_tag(full_file_buf, 0x03, int_to_vector(1));
+	add_tag(full_file_buf, 0x04, string_to_vector("App name"));
+	add_tag(full_file_buf, 0x05, int_to_vector(0x010000));
+	add_tag(full_file_buf, 0x0F, int_to_vector(512));
+	add_tag(full_file_buf, 0x10, int_to_vector(0));
+	add_tag(full_file_buf, 0x11, int_to_vector(0x9C400000));
+	add_tag(full_file_buf, 0x12, string_to_vector("1234567890"));
+	add_tag(full_file_buf, 0x16, int_to_vector(1));
+	add_tag(full_file_buf, 0x18, int_to_vector(0));
+	//add_tag(full_file_buf, 0x19, mstring_to_vector("App name"));
+	add_tag(full_file_buf, 0x1C, int_to_vector(0));
+	add_tag(full_file_buf, 0x21, int_to_vector(6));
+	add_tag(full_file_buf, 0x22, int_to_vector(0));
+	add_tag(full_file_buf, 0x23, int_to_vector(0));
+	add_tag(full_file_buf, 0x25, int_to_vector(0));
+	add_tag(full_file_buf, 0x29, int_to_vector(0));
+	add_tag(full_file_buf, 0x00, vector<byte>());
+
+	//Add end
+	add_end(full_file_buf, tag_pos);
+
+	ofstream out(out_path, ios_base::binary);
+	if (!out.good())
+		return 0;
+	out.write((char*)full_file_buf.data(), full_file_buf.size());
+	out.close();
 }
 
-void fix_res_offsets(vector<byte> &buf, size_t res_offset, size_t res_size)
+void fix_res_offsets(vector<byte>& buf, size_t res_offset, size_t res_size)
 {
 	size_t pos = res_offset;
 
@@ -79,7 +119,7 @@ void fix_res_offsets(vector<byte> &buf, size_t res_offset, size_t res_size)
 			break;
 
 
-		long &offset = *(long*)(buf.data() + pos);
+		long& offset = *(long*)(buf.data() + pos);
 		pos += 4;
 
 		long size = *(long*)(buf.data() + pos);
@@ -88,7 +128,7 @@ void fix_res_offsets(vector<byte> &buf, size_t res_offset, size_t res_size)
 		offset += res_offset;
 	}
 
-	long &res2_offset = *(long*)(buf.data() + pos);
+	long& res2_offset = *(long*)(buf.data() + pos);
 	res2_offset += res_offset;
 
 	pos = res2_offset;
@@ -101,10 +141,58 @@ void fix_res_offsets(vector<byte> &buf, size_t res_offset, size_t res_size)
 		pos += 4;
 
 		offset += res_offset;
-		
+
 		if (id == 0xFFFFFFFF)
 			break;
 	}
 }
 
+void add_vector(vector<byte>& a, const vector<byte>& b)
+{
+	a.insert(a.end(), b.begin(), b.end());
+}
 
+void add_tag(vector<byte>& buf, long id, const vector<byte>& data)
+{
+	vector<byte> tag(8);
+	tag.resize(8);
+	*(long*)(tag.data()) = id;
+	*(long*)(tag.data() + 4) = data.size();
+	add_vector(buf, tag);
+	add_vector(buf, data);
+}
+
+vector<byte> string_to_vector(string name)
+{
+	vector<byte> str(4 + name.size() + 1);
+	memcpy(str.data() + 4, name.c_str(), name.size() + 1);
+	*(long*)(str.data()) = name.size() + 1;
+	return str;
+}
+
+vector<byte> int_to_vector(unsigned long num)
+{
+	vector<byte> n(4);
+	*(unsigned long*)(n.data()) = num;
+	return n;
+}
+
+vector<byte> mstring_to_vector(string name)
+{
+	vector<byte> str;
+	for (int i = 0; i < 3; ++i) {
+		add_vector(str, int_to_vector(i+1));
+		add_vector(str, string_to_vector(name));
+	}
+		
+	return str;
+}
+
+void add_end(vector<byte>& buf, long tags_pos)
+{
+	vector<byte> end(0x56);
+	unsigned char st_bytes[7] = { 0xB4, 0x56, 0x44, 0x45, 0x31, 0x30, 0x1 };
+	memcpy(end.data(), st_bytes, 7);
+	*(long*)(end.data() + 0x56 - 12) = tags_pos;
+	add_vector(buf, end);
+}
